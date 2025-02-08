8054db8f59505b6b3c8de1aa0652e7516e0a736c
morkof <morkof@gmail.com>
2025-02-08 02:19:35 +0100

    install chrome script

---
+++ scripts/install-chrome.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Add Google Chrome repository
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list
+
+# Update package list and install Google Chrome
+apt-get update
+apt-get install -y google-chrome-stable
